7ed9ca8d4bc5690b5d35803f72f0bd6fb9b0a2b4
Sahu, Gaurav <hashcode.dev@gmail.com>
2025-05-24 18:46:50 +0530

    [INT-17]: WAP for Java Vector Demo

--- src/main/java/com/javacoreconcepts/datastructures/VectorDemo.java
+++ src/main/java/com/javacoreconcepts/datastructures/VectorDemo.java
@@ -5,11 +5,23 @@ import java.util.Vector;
 public class VectorDemo {
     public static void main(String[] args) {
         Vector<Integer> integerVector = new Vector<>();
+        //Add operations
         integerVector.add(1);
         integerVector.add(2);
         integerVector.add(3);
         integerVector.add(4);
         integerVector.add(5);
-        System.out.println("Vector Demo: " + integerVector);
+        System.out.println("Vector Add Object Demo: " + integerVector);
+        //Update operation
+        integerVector.set(0,12);
+        System.out.println("Vector Update Object Demo: " + integerVector);
+        //Delete operation
+        integerVector.remove(1);
+        integerVector.remove(Integer.valueOf(2));
+        System.out.println("Vector Delete Object Demo: " + integerVector);
+        //Iterations
+        integerVector.forEach(integer -> {
+            System.out.println("Element:" + integer);
+        });
     }
 }
