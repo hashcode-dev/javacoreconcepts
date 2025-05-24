4ac245cf0261fc6b1ff6ed2ddf131fca83f2de4a
Sahu, Gaurav <hashcode.dev@gmail.com>
2025-05-24 16:33:30 +0530

    [INT-19]: WAP explaining ArrayList in Java

---
+++ src/main/java/com/javacoreconcepts/datastructures/ArrayListDemo.java
@@ -0,0 +1,14 @@
+package com.javacoreconcepts.datastructures;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Gaurav");
+        arrayList.add("Prakhar");
+        arrayList.add("Priyanka");
+        arrayList.add("Shrisha");
+        System.out.println("ArrayList: " + arrayList);
+    }
+}
